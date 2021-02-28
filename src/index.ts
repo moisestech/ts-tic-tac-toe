b637e45fa14f8ddab47b2345c1d3b33af01e8a76
moisestech <m@moises.tech>
2021-02-28 06:56:54 -0500

    comment js functionality

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 import "./style.css";
 
+// start with a couple of elements that we get off the page using element by ID
 const appElement = document.getElementById("app");
 const boardElement = document.getElementById("board");
+
+// constants to represent the number of ROWs and COLLUMNs that are on the board
 const ROW_COUNT = 3;
 const COL_COUNT = 3;
 
+// we have a boardState variable which holds wether there is an X or an O in each of the Cells 
 let boardState = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""]
 ];
 
+// another variable determining who's turn it is
 let currentMove = "X";
 
+// a function that creates each of these cells
+// takes in the row and the collumn which the cell is positined in and the content of the cell
 function createCell(row, col, content = "") {
   const cell = document.createElement("button");
   cell.setAttribute("data-row", row.toString());
   cell.setAttribute("data-col", col.toString());
   cell.setAttribute("data-content", content);
   cell.classList.add("cell");
+
+  // return the cell is attributes so that we can use it in the renderBoard function
   return cell;
 }
 
@@ -27,12 +36,15 @@ function renderBoard() {
   if (!boardElement) throw new Error("Cannot find board");
   boardElement.innerHTML = "";
   
+  // Made in a way that it removes everything from the DOM and builds it from scratch.
+  // We loop through the rows and columns and we add the new cell
   for (let i = 0; i < ROW_COUNT; i++) {
     for (let j = 0; j < COL_COUNT; j++) {
       boardElement.appendChild(createCell(i, j, boardState[i][j]));
     }
   }
 
+  // decides who's turned is it wether X or O
   const oldMoveElement = document.getElementById("move-element");
   if (oldMoveElement) { 
     oldMoveElement.remove();
@@ -45,6 +57,7 @@ function renderBoard() {
   appElement.insertBefore(moveElement, document.getElementById("reset"));
 }
 
+// resets the game
 function init() {
   const resetButton = document.getElementById("reset");
   if (! resetButton) throw new Error("No Reset button");
@@ -62,6 +75,9 @@ function init() {
   renderBoard();
 }
 
-// https://codesandbox.io/s/github/uidotdev/typescript-tic-tac-toe
+// generates a reset button at the beginning of the game 
+// and renders our board for the first time
+init();
 
+// https://codesandbox.io/s/github/uidotdev/typescript-tic-tac-toe
 // https://platform.ui.dev/courses/1207737/lectures/26978324
